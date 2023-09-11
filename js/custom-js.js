0345d8f365a39ffd6c3ac8f1ca6c8db752b9964d
Kevin Morco <christophermorco02@gmail.com>
2023-09-11 19:24:45 +0800

    Update

--- js/custom-js.js
+++ js/custom-js.js
@@ -5,6 +5,8 @@ let element = document.getElementById('typewriter-text')
 let currentStringIndex = 0
 let currentCharIndex = 0
 
+// Type writer effect for span
+
 let typeWriter = () => {
     let currentString = textArray[currentStringIndex]
     if (currentCharIndex < currentString.length) {
@@ -30,77 +32,117 @@ let typeWriter = () => {
 
 typeWriter();
 
-const navbar = document.getElementById('navbar');
-let scroll = 0
-window.addEventListener('scroll', () => {
-    scroll = window.scrollY
-    if (scroll > 1) {
-        navbar.classList.add('navbar-sticky')
+// Enable sticky value on navbar oscroll
 
-    } else {
-        navbar.classList.remove('navbar-sticky')
+const stickyNavbar = () => {
+    const navbar = document.getElementById('navbar');
+    let scroll = 0
+    window.addEventListener('scroll', () => {
+        scroll = window.scrollY
+        if (scroll > 1) {
+            navbar.classList.add('navbar-sticky')
 
-    }
-});
+        } else {
+            navbar.classList.remove('navbar-sticky')
 
-let button = document.querySelectorAll('.experience-section__button')
-let div = document.querySelectorAll('.experience-section__work-content')
-
-button.forEach((buttonEl, index) => {
-    buttonEl.addEventListener('click', () => {
-        button.forEach((btn) => {
-            btn.classList.remove('experience-section__btn-group--active');
-        });
-
-        // Add the "clicked" class to the clicked button
-        buttonEl.classList.add('experience-section__btn-group--active');
-
-        div.forEach((divs) => {
-            divs.style.display = 'none'
-        })
-        // Show the corresponding div based on the button's index
-        div[index].style.display = 'block';
-    })
-})
-
-const hamburger = document.querySelectorAll('.eye-icon-container')
-const eye = document.querySelectorAll('.eye-icon-container i')
-const navListPhone = document.querySelectorAll('.nav-list-phone-container')
-hamburger.forEach((hamburgerEl) => {
-    hamburgerEl.addEventListener('click', () => {
-        eye.forEach((eyeEl) => {
-            if (eyeEl.classList.contains('fa-eye-slash')) {
-                eyeEl.classList.remove('fa-eye-slash')
-                eyeEl.classList.add('fa-eye')
-
-            } else {
-                eyeEl.classList.remove('fa-eye')
-                eyeEl.classList.add('fa-eye-slash')
-            }
-
-        })
-
-        navListPhone.forEach((navListPhoneEl) => {
-            navListPhoneEl.classList.toggle('hidden')
-
-            if (navListPhoneEl.classList.contains('hidden')) {
-                console.log('It contains the hidden class')
-            } else {
-                navListPhoneEl.style.opacity = 1
-            }
-        })
-
-    })
-})
-
-let icon = document.querySelectorAll('.external-link')
-
-icon.forEach((iconEl)=>{
-    iconEl.addEventListener('click', (event) =>{
-        if(confirm('You are about to open this link to a new tab, proceed?') === false){
-            event.preventDefault();
         }
+    });
+}
+stickyNavbar()
+
+// Toggle work button to show and hide containers
+
+const toggleWorkButton = () => {
+    let button = document.querySelectorAll('.experience-section__button')
+    let div = document.querySelectorAll('.experience-section__work-content')
+
+    button.forEach((buttonEl, index) => {
+        buttonEl.addEventListener('click', () => {
+            button.forEach((btn) => {
+                btn.classList.remove('experience-section__btn-group--active');
+            });
+
+            buttonEl.classList.add('experience-section__btn-group--active');
+
+            div.forEach((divs) => {
+                divs.style.display = 'none'
+            })
+            // Show the corresponding div based on the button's index
+            div[index].style.display = 'block';
+        })
     })
-})
+}
 
+toggleWorkButton()
 
+// Change phone navbar icon on click
+
+const navIconEffect = () => {
+    const hamburgerIcon = document.querySelectorAll('.eye-icon-container')
+    const eyeIcon = document.querySelectorAll('.eye-icon-container i')
+    const navListPhone = document.querySelectorAll('.navbar-header__nav-list-phone-container')
+    hamburgerIcon.forEach((hamburgerEl) => {
+        hamburgerEl.addEventListener('click', () => {
+            eyeIcon.forEach((eyeEl) => {
+                if (eyeEl.classList.contains('fa-eye-slash')) {
+                    eyeEl.classList.remove('fa-eye-slash')
+                    eyeEl.classList.add('fa-eye')
+
+                } else {
+                    eyeEl.classList.remove('fa-eye')
+                    eyeEl.classList.add('fa-eye-slash')
+                }
+
+            })
+
+            navListPhone.forEach((navListPhoneEl) => {
+                navListPhoneEl.classList.toggle('hidden')
+
+                if (navListPhoneEl.classList.contains('hidden')) {
+                    console.log('It contains the hidden class')
+                } else {
+                    navListPhoneEl.style.opacity = 1
+                }
+            })
+        })
+    })
+}
+
+navIconEffect()
+
+// Confirmation display when user clicks on a link
+
+const alertLink = () => {
+    let icon = document.querySelectorAll('.external-link')
+
+    icon.forEach((iconEl) => {
+        iconEl.addEventListener('click', (event) => {
+            event.preventDefault()
+            Swal.fire({
+                title: 'This link will open to a new tab',
+                text: "Do you want to proceed?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                customClass: {
+                    cancelButton: 'custom-cancel-button',
+                },
+                confirmButtonText: 'Proceed',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = iconEl.getAttribute('href')
+
+                } else {
+                    event.preventDefault();
+                }
+            })
+            // if (confirm('You are about to open this link to a new tab, proceed?') === false) {
+            //     event.preventDefault();
+            // }
+
+        })
+    })
+}
+
+alertLink()
