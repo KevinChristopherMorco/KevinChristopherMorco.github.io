cd3277cf5d097ae30b7581530695827ac3023251
Kevin Morco <christophermorco02@gmail.com>
2023-10-03 19:38:25 +0800

    new

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,5 +1,5 @@
 new WOW().init();
-let textArray = ['Web Developer', 'Graphic Designer', 'Programmer'];
+let textArray = ['Web Developer','Programmer'];
 let element = document.getElementById('typewriter-text')
 
 let currentStringIndex = 0
@@ -72,8 +72,52 @@ const toggleWorkButton = () => {
         })
     })
 }
-
 toggleWorkButton()
+
+
+const nightMode = () => {
+    let modeIcon = document.querySelectorAll('.mode-icon-container i')
+    let root = document.querySelector(':root')
+    const linkedin = document.querySelector('.fa-linkedin')
+    modeIcon.forEach((modeIconEl) => {
+        modeIconEl.addEventListener('click', (e) => {
+
+            if (modeIconEl.classList.contains('fa-moon')) {
+                // set to nightmode
+
+                modeIconEl.classList.remove('fa-moon')
+                modeIconEl.classList.add('fa-sun')
+                document.documentElement.style.setProperty('--primary-color', '#000');
+                document.documentElement.style.setProperty('--foreground-color', '#fff');
+                document.documentElement.style.setProperty('--linkedin', '#fff');
+                document.documentElement.style.setProperty('--github', '#fff');
+                document.documentElement.style.setProperty('--facebook', '#fff');
+                document.documentElement.style.setProperty('--span-text', '#fff')
+                document.documentElement.style.setProperty('--footer', '#000')
+
+            } else {
+                //set to lightmode
+
+                modeIconEl.classList.add('fa-moon')
+                modeIconEl.classList.remove('fa-sun')
+               
+
+                document.documentElement.style.setProperty('--primary-color', '#F7F7F7');
+                document.documentElement.style.setProperty('--foreground-color', '#000');
+                document.documentElement.style.setProperty('--linkedin', '#0073B2');
+                document.documentElement.style.setProperty('--github', '#000');
+                document.documentElement.style.setProperty('--facebook', '#0866FF');
+                document.documentElement.style.setProperty('--span-text', '#4976E7')
+                document.documentElement.style.setProperty('--footer', '#4976E7')
+
+
+            }
+        })
+    })
+}
+
+nightMode()
+
 
 // Change phone navbar icon on click
 
@@ -132,7 +176,7 @@ const alertLink = () => {
                 reverseButtons: true
             }).then((result) => {
                 if (result.isConfirmed) {
-                    window.location.href = iconEl.getAttribute('href')
+                    window.open(iconEl.getAttribute('href'), '_blank');
 
                 } else {
                     event.preventDefault();
